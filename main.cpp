1f417f89f1eedd2fe6cc1ac6ba772dc0dd6325db
Ian McQueen <mcqueenian666@gmail.com>
2020-07-12 18:54:51 -0400

    Finished input validation for construction of ANN

--- main.cpp
+++ main.cpp
@@ -16,11 +16,31 @@ int highestoutput(FeedForward f){
  return max;
 }
 
-int main(char argv[], int args){
- if(args != 0){
-  std::cout << "No arguments please." << endl;
+int main(int argc, char **argv){
+ if(argc != 1){
+  std::cout << "No arguments please." << std::endl;
   return 0;
  }
- std::cout << "Deep learning with ANN, an evolutionary approach. Compiled on " __DATE__ << endl;
+ std::cout << "Interactive deep learning with ANN, first with an evolutionary approach. Compiled on " __DATE__ " at " __TIME__ "." << std::endl;
+ std::cout << "To begin we must construct the neural network. Input and output node count are 784 and 10 respectively, fixed by implementation" << std::endl;
+ std::cout << "However, you must set the dimensions of the hidden portion of the network. How many hidden layers should it have?" << std::endl;
+ unsigned int layers, nperl;//Number of hidden layers, and number of nodes per layer
+ int t;//temp for data validation
+ while(std::cout << "Enter a positive integer." && !(std::cin >> t) || t <= 0 ){
+  std::cin.clear();
+  std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+  std::cout << "Caught invalid data. Try again." << std::endl;
+ }
+ layers = t;
+ std::cout << "Now how many nodes in each hidden layer?" << std::endl;
+ while(std::cout << "Enter a positive integer." && !(std::cin >> t) || t <= 0 ){
+  std::cin.clear();
+  std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+  std::cout << "Caught invalid data. Try again." << std::endl;
+ }
+ nperl = t;
+ std::cout << "Constructing network with the given specifications." << std::endl << layers << " is the number of hidden layers and " << nperl << " is the number of nodes in each hidden layer." << std::endl;
+ FeedForward trainee(784, layers, nperl, 10);
+ std::cout << "Trainee network constructed!" << std::endl;
  return 0;
 }
--- setup.sh
+++ setup.sh
@@ -1,7 +1,23 @@
-#!/bin/bash
-if [ ! -d "$Training" ]; then
+#!/usr/bin/env bash
+trainingmade=true
+testmade=true
+if [ ! -d "Training" ]; then
  mkdir ./Training
+ trainingmade=false
 fi
-if [ ! -d "$Test" ]; then
- mkdir ./Training
+if [ ! -d "Test" ]; then
+ mkdir ./Test
+ testmade=false
+fi
+if [ "$trainingmade" = false ]; then
+ cp ./MNISTdata/train-images-idx3-ubyte.gz ./Training/train-images-idx3-ubyte.gz
+ cp ./MNISTdata/train-labels-idx1-ubyte.gz ./Training/train-labels-idx1-ubyte.gz
+ gunzip ./Training/train-images-idx3-ubyte.gz
+ gunzip ./Training/train-labels-idx1-ubyte.gz
+fi
+if [ "$testmade" = false ]; then
+ cp ./MNISTdata/t10k-images-idx3-ubyte.gz ./Test/t10k-images-idx3-ubyte.gz
+ cp ./MNISTdata/t10k-labels-idx1-ubyte.gz ./Test/t10k-labels-idx1-ubyte.gz
+ gunzip ./Test/t10k-images-idx3-ubyte.gz
+ gunzip ./Test/t10k-labels-idx1-ubyte.gz
 fi
